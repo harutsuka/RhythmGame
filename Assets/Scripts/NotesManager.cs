65497837182b16ea80b938cca7a2770664dedad9
haru <harutsuka0427@gmail.com>
2024-02-09 23:00:46 +0900

    add: MIDI Animation Track

--- Assets/Scripts/NotesManager.cs
+++ Assets/Scripts/NotesManager.cs
@@ -3,61 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable]
-public class Data
-{
-    public string name;
-    public int maxBlock;
-    public int BPM;
-    public int offset;
-    public Note[] notes;
-
-}
-[Serializable]
-public class Note
-{
-    public int type;
-    public int num;
-    public int block;
-    public int LPB;
-}
-
 public class NotesManager : MonoBehaviour
 {
-    public int noteNum;
-    private string songName;
-    public List<int> LaneNum = new List<int>();
-    public List<int> NoteType = new List<int>();
-    public List<float> NotesTime = new List<float>();
-    public List<GameObject> NotesObject = new List<GameObject>();
-
-    [SerializeField] private float NotesSpeed;
-    [SerializeField] GameObject noteObj;
-
-    private void OnEnable()
-    {
-        noteNum = 0;
-        songName = "test";
-        Load(songName);
-    }
-    private void Load(string SongName)
-    {
-        string inputString = Resources.Load<TextAsset>(songName).ToString();
-        Data inputJson = JsonUtility.FromJson<Data>(inputString);
-
-        noteNum = inputJson.notes.Length;
-
-        for (int i = 0; i < inputJson.notes.Length; i++)
-        {
-            float interval = 60 / (inputJson.BPM * (float)inputJson.notes[i].LPB);
-            float beatSec = interval * (float)inputJson.notes[i].LPB;
-            float time = (beatSec * inputJson.notes[i].num / (float)inputJson.notes[i].LPB) + inputJson.offset + 0.01f;
-            NotesTime.Add(time);
-            LaneNum.Add(inputJson.notes[i].block);
-            NoteType.Add(inputJson.notes[i].type);
-
-            float z = NotesTime[i] * NotesSpeed;
-            NotesObject.Add(Instantiate(noteObj, new Vector3(inputJson.notes[i].block - 1.5f, 0.55f, z), Quaternion.identity));
-        }
+    public GameObject notes;
+    
+    public void CreateNotes(){
+        Instantiate(notes,new Vector3(0.5f,0.12f,7.61f),Quaternion.identity);
     }
 }
