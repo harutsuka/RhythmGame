f91aaa81798814cb7096a01b37cfeb97a7359ea2
haru <harutsuka0427@gmail.com>
2024-03-18 11:15:47 +0900

    add: star.mp3

--- Assets/Scripts/Notes.cs
+++ Assets/Scripts/Notes.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Notes : MonoBehaviour
 {
     public float noteSpeed = 5f;
-    public int score;
+
+    public GameObject perfectText;
+    public Vector3 accuracyPosition = new Vector3(0,-0.4f,0.8f);
+    public Quaternion rotate = Quaternion.Euler(76, -0.5f, 0);
 
     public float perfectTiming = 0.1f;
     public float greatTiming = 0.2f;
@@ -24,23 +28,19 @@ public class Notes : MonoBehaviour
             
             if (timingDifference <= perfectTiming)
             {
-                Debug.Log("Perfect");
-                score += 200;
+                //Instantiate(perfectText,accuracyPosition,rotate);
             }
             else if (timingDifference <= greatTiming)
             {
                 Debug.Log("Great");
-                score += 150;
             }
             else if (timingDifference <= goodTiming)
             {
                 Debug.Log("Good");
-                score += 100;
             }
             else if (timingDifference <= badTiming)
             {
                 Debug.Log("Bad");
-                score += 50;
             }
             else Debug.Log("Miss");
             Destroy(gameObject);
--- Assets/Scripts/NotesManager.cs
+++ Assets/Scripts/NotesManager.cs
@@ -6,8 +6,17 @@ using UnityEngine;
 public class NotesManager : MonoBehaviour
 {
     public GameObject notes;
+    [SerializeField] int LaneNumber;
     
     public void CreateNotes(){
-        Instantiate(notes,new Vector3(0.5f,0.12f,11f),Quaternion.identity);
+        if(LaneNumber == 0){
+            Instantiate(notes,new Vector3(-1.5f,0.06f,11f),Quaternion.identity);
+        }else if(LaneNumber == 1){
+            Instantiate(notes,new Vector3(-0.5f,0.06f,11f),Quaternion.identity);
+        }else if(LaneNumber == 2){
+            Instantiate(notes,new Vector3(0.5f,0.06f,11f),Quaternion.identity);
+        }else{
+            Instantiate(notes,new Vector3(1.5f,0.06f,11f),Quaternion.identity);
+        }
     }
 }
