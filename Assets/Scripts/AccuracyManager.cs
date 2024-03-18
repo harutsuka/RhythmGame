fbf3347db37bb166a75d752a3f8493412c21d601
haru <harutsuka0427@gmail.com>
2024-03-19 00:30:17 +0900

    update: AccuracyManager.cs

--- Assets/Scripts/AccuracyManager.cs
+++ Assets/Scripts/AccuracyManager.cs
@@ -7,19 +7,13 @@ public class AccuracyManager : MonoBehaviour
 {
     public GameObject judgeLine;
     public GameObject note;
-    public Vector3 accuracyPosition = new Vector3(0,-0.4f,0.8f);
-    public Quaternion rotate = Quaternion.Euler(76, -0.5f, 0);
-
-    public Text perfectText;
-    public Text greatText;
-    public Text goodText;
-    public Text badText;
-    public Text missText;
+    
+    public Text accuracyText;
     public float perfectTiming = 0.1f;
     public float greatTiming = 0.2f;
     public float goodTiming = 0.3f;
     public float badTiming = 0.5f;
-    
+      
     void Update()
     {
         float position = transform.position.z;
@@ -40,11 +34,14 @@ public class AccuracyManager : MonoBehaviour
         float playerPosition = 0f;
         float timingDifference = Mathf.Abs(position - playerPosition);
 
-        if(timingDifference <= perfectTiming) Debug.Log("Perfect");
+        if(timingDifference <= perfectTiming) Debug.Log("perfect");
         else if(timingDifference <= greatTiming) Debug.Log("Great");
         else if(timingDifference <= goodTiming) Debug.Log("Good");
         else if(timingDifference <= badTiming) Debug.Log("Bad");
         else Debug.Log("Miss!");
+
+        //ひとつひとつテキスト作らんでも判定によってテキストの内容変えたらよくね？？？？？
+        
 
         Destroy(gameObject);
     }
