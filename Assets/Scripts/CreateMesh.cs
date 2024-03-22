b6298a103b783e8749308d278632532c96b50cc5
haru <harutsuka0427@gmail.com>
2024-03-22 22:08:51 +0900

    update: ロングノーツを実装するためmesh作成関数をJsonLoader.csに転記

--- Assets/Scripts/CreateMesh.cs
+++ Assets/Scripts/CreateMesh.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class CreateMesh : MonoBehaviour
 {
-    public Material material;
+   
     // Start is called before the first frame update
     void Start()
     {
-        GenerateMesh();
+        
     }
 
     // Update is called once per frame
@@ -16,24 +16,5 @@ public class CreateMesh : MonoBehaviour
     {
         
     }
-    void GenerateMesh(){
-        MeshFilter mf = GetComponent<MeshFilter>();
-        MeshRenderer mr = GetComponent<MeshRenderer>();
- 
-        Vector3[] verts = new Vector3[4];
-        int[] triangles = { 0, 1, 3, 1, 2, 3 };
- 
-        verts[0] = new Vector3(-1f, 0.06f, 5);
-        verts[1] = new Vector3(0, 0.06f, 5);
-        verts[2] = new Vector3(0, 0, 5);
-        verts[3] = new Vector3(-1f, 0, 5);
- 
-        Mesh mesh = new Mesh();
-        mesh.vertices = verts;
-        mesh.triangles = triangles;
-        mesh.RecalculateNormals();
- 
-        mf.sharedMesh = mesh;
-        mr.sharedMaterial = material;
-    }
+    
 }
--- Assets/Scripts/JsonLoader.cs
+++ Assets/Scripts/JsonLoader.cs
@@ -6,6 +6,8 @@ public class JsonLoader : MonoBehaviour
 {
     public TextAsset textAsset;
     public GameObject notes;
+
+     public Material material;
     // Start is called before the first frame update
     void Start()
     {
@@ -50,10 +52,12 @@ public class JsonLoader : MonoBehaviour
                 }else if(name == "F4")
                 {
                     Invoke("GenerateF4Note",endTime);
-                }      
+                }
+                GenerateMesh();
             }
         }
         
+        GameStart();
     }
     void GenerateC4Note(){
         GenerateNote("C4");
@@ -81,5 +85,30 @@ public class JsonLoader : MonoBehaviour
             position = new Vector3(1.5f,0.06f,11f);
         }
         Instantiate(notes,position,Quaternion.identity);
+    }
+
+    void GenerateMesh(){
+        MeshFilter mf = GetComponent<MeshFilter>();
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+ 
+        Vector3[] verts = new Vector3[4];
+        int[] triangles = { 0, 1, 3, 1, 2, 3 };
+ 
+        verts[0] = new Vector3(-1f, 0.06f, 11);
+        verts[1] = new Vector3(0, 0.06f,11);
+        verts[2] = new Vector3(0, 0.06f, 0);
+        verts[3] = new Vector3(-1f, 0.06f, 0);
+ 
+        Mesh mesh = new Mesh();
+        mesh.vertices = verts;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+ 
+        mf.sharedMesh = mesh;
+        mr.sharedMaterial = material;
+    }
+
+    void GameStart(){
+        
     }
 }
